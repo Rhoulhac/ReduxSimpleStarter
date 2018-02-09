d8efccc754cd8b7a901757a94a718e17a6999997
Rhoulhac <greg.rhoulhac@gmail.com>
2018-02-09 09:26:56 -0600

    Add Search bar without wiring it up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyB9X-anVYscqA-BHcojr9IyaQc1y4sGunI';
+
 // Create a new component that produces some HTML
 const App = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take the component's generated HTML and put it on the page
