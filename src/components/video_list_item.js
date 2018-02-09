73907d2213fd0898330456e6042a6cbe5f828482
Rhoulhac <greg.rhoulhac@gmail.com>
2018-02-09 13:02:14 -0600

    Add real detail about videos and embed video

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Some Video</li>
+// ES6 syntatical sugar to deconstruct video from props (like props.video)
+const VideoListItem = ({video}) => {
+  const imageURL = video.snippet.thumbnails.default.url;
+
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyB9X-anVYscqA-BHcojr9IyaQc1y4sGunI';
 
@@ -22,6 +23,7 @@ constructor(props) {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
